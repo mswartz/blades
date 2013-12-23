7dedfec663323a0b099bac137de3506b61c04beb
Mike Swartz <mswartz@gmail.com>
2013-12-23 17:21:15 -0500

    adding 404 route

---
+++ main.js
@@ -0,0 +1,51 @@
+/**********************************
+
+M A I N
+
+**********************************/
+
+// Set up collections for 
+Players = new Meteor.Collection("players");
+Games = new Meteor.Collection("games");
+
+
+if (Meteor.isClient) {
+
+  Router.configure({
+    notFoundTemplate: 'notFound' // this will render
+  });
+  
+  //Iron Router routings
+  Router.map(function () {
+    //Home template
+    this.route('home', {
+      path: '/',
+      template: 'home'
+    });
+
+    //Add a game
+    this.route('newgame', {
+      path: '/newgame',
+      template: 'newgame'
+    });
+
+    //Games
+    this.route('games', {
+      path: '/games',
+      template: 'games'
+    });
+
+    //Players
+    this.route('players', {
+      path: '/players',
+      template: 'players'
+    });
+  });
+
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
--- routes.js
+++ routes.js
@@ -1,27 +1,3 @@
 if (Meteor.isClient) {
-  Router.map(function () {
-    //Home template
-    this.route('home', {
-      path: '/',
-      template: 'home'
-    });
 
-    //Add a game
-    this.route('newgame', {
-      path: '/newgame',
-      template: 'newgame'
-    });
-
-    //Games
-    this.route('games', {
-      path: '/games',
-      template: 'games'
-    });
-
-    //Players
-    this.route('players', {
-      path: '/players',
-      template: 'players'
-    });
-  });
 }
