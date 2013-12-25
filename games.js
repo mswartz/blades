5f9a8260f9504bb4f14ef42dae87522e66987ccb
Mike Swartz <mswartz@gmail.com>
2013-12-24 23:22:04 -0500

    connecting live Player ids to the Games.html output

--- games.js
+++ games.js
@@ -13,7 +13,19 @@ if (Meteor.isClient) {
 Template.games.helpers({
   games : function(){
     var games = Games.find({}).fetch();
+
+    for(var i = 0; i<games.length; i++){
+    	var p1 = Players.find({_id:games[i].p1_id});
+    	var p2 = Players.find({_id:games[i].p2_id});
+    	games[i].p1 = p1;
+    	games[i].p2 = p2;
+    }
+
     return games;
+  },
+
+  total_games : function(){
+  	return Games.find().count();
   }
 });
 
