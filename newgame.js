93251df15b0b4cf5ce7874685cf288b18c3697a4
Mike Swartz <mswartz@gmail.com>
2013-12-24 22:56:28 -0500

    connecting playernames to ids and such in newgame

--- newgame.js
+++ newgame.js
@@ -14,6 +14,9 @@ Session.setDefault('p2_pts', 0);
 Session.setDefault('p1_name', 'Player 1');
 Session.setDefault('p2_name', 'Player 2');
 
+Session.setDefault('p1_id', 'Player1');
+Session.setDefault('p2_id', 'Player2');
+
 
 Template.newgame.helpers({
   players : function(){
@@ -35,12 +38,26 @@ Template.newgame.helpers({
     return Session.get('p2_pts');
   },
 
+  p1_id : function() {
+    return Session.get('p1_id');
+  },
+
+  p2_id : function() {
+    return Session.get('p2_id');
+  },
+
   p1_name : function() {
-    return Session.get('p1_name');
+    var p1_name = Players.findOne({_id:Session.get('p1_id')});
+    if(p1_name){
+      return p1_name.name;
+    }
   },
 
   p2_name : function() {
-    return Session.get('p2_name');
+    var p2_name = Players.findOne({_id:Session.get('p2_id')});
+    if(p2_name){
+      return p2_name.name;
+    }
   },
 
   all_names : function() {
@@ -66,42 +83,49 @@ Template.newgame.events({
   },
 
   'blur select.name' : function() {
-    Session.set('p1_name', $('#p1_name').val());
-    Session.set('p2_name', $('#p2_name').val());
+    Session.set('p1_id', $('#p1_name').val());
+    Session.set('p2_id', $('#p2_name').val());
   },
 
   'click input#newgame_addgame' : function() {
     var game_no = Games.find().count() + 1;
 
-    var p1_name = $('#newgame_p1_name').val();
-    var p2_name = $('#newgame_p2_name').val();
+    var p1_team = $('#newgame_p1_team').val();
+    var p2_team = $('#newgame_p2_team').val();
 
-    var p1_team = $('#newgame_p1_name').val();
-    var p2_team = $('#newgame_p1_name').val();
-
-    var p1_game_goals = {
-      '1p' : $('#newgame_p1_name').val(),
-      '2p' : $('#newgame_p1_name').val(),
-      '3p' : $('#newgame_p1_name').val()
+    var p1_reg_goals = {
+      '1p' : $('#newgame_p1_score_1p').val(),
+      '2p' : $('#newgame_p1_score_2p').val(),
+      '3p' : $('#newgame_p1_score_3p').val()
     };
 
-    var p2_game_goals = {
-      '1p' : $('#newgame_p1_name').val(),
-      '2p' : $('#newgame_p1_name').val(),
-      '3p' : $('#newgame_p1_name').val()
-    };
-
-    var p1_ot_goals = {
-      '1p' : $('#newgame_p1_name').val(),
-      '2p' : $('#newgame_p1_name').val(),
-      '3p' : $('#newgame_p1_name').val()
+    var p2_reg_goals = {
+      '1p' : $('#newgame_p2_score_1p').val(),
+      '2p' : $('#newgame_p2_score_2p').val(),
+      '3p' : $('#newgame_p2_score_3p').val()
     };
 
     var p1_fights = $('#newgame_p1_fights').val();
-    var p2_fights = $('#newgame_p1_fights').val();
+    var p2_fights = $('#newgame_p2_fights').val();
 
+    var p1_final = $('#newgame_p1_final').val();
+    var p2_final = $('#newgame_p2_final').val();
 
-    Games.insert(newgame);
+    Games.insert({
+      'game_no' : game_no,
+
+      'p1_id' : Session.get('p1_id'),
+      'p2_id' : Session.get('p2_id'),
+
+      'p1_reg_goals' : p1_reg_goals,
+      'p2_reg_goals' : p2_reg_goals,
+
+      'p1_fights' : p1_fights,
+      'p2_fights' : p2_fights,
+
+      'p1_final' : p1_final,
+      'p2_final' : p2_final
+    });
   }
 });
 
