3330ca6fe91e4120e2e13d0bc16932302012edc8
Mike Swartz <mswartz@gmail.com>
2013-12-27 17:07:55 -0500

    adding in the scaffold for Player records on player_detail

--- player_detail.js
+++ player_detail.js
@@ -10,6 +10,9 @@ if (Meteor.isClient) {
 	Template.player_detail.helpers({
 	  player_id : function(){
 	  	return Session.get('player_id');
+	  },
+	  player : function(){
+	  	return Players.find({_id : Session.get('player_id')}).fetch();
 	  }
 	});
 }
