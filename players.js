87a9b03bf14a0a67054e9782c9d55898f878c95f
Mike Swartz <mswartz@gmail.com>
2013-12-28 22:21:08 -0500

    restructuring players.html

--- players.js
+++ players.js
@@ -14,6 +14,10 @@ Template.players.helpers({
   players : function(){
     var players = Players.find({}).fetch();
     return players;
+  },
+  scorers : function(){
+    var scorers = Players.find({}, {sort: {goals_scored : -1}, limit : 5}).fetch();
+    return scorers;
   }
 });
 
