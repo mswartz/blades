54b09f4725848f6c9966f9ff7e5d3d46d0bcc77d
Mike Swartz <mswartz@gmail.com>
2014-01-01 14:14:55 -0500

    adding dynamic results per page field

--- games.js
+++ games.js
@@ -12,6 +12,8 @@ if (Meteor.isClient) {
 
 Session.setDefault('sort_by', 'game_no_asc');
 Session.setDefault('game_range', 1);
+Session.setDefault('results_num', 50);
+
 
 Template.games.helpers({
   games : function(){
@@ -28,6 +30,15 @@ Template.games.helpers({
 
   total_games : function(){
   	return Games.find().count();
+  },
+
+  game_range : function(){
+    var range = [];
+    
+    range[0] = {range_lower: 1, range_upper: 50 };
+    range[1] = {range_lower: 51, range_upper: 100 };
+
+    return range;
   }
 });
 
@@ -41,6 +52,10 @@ Template.games.events({
   'change #game_range' : function() {
     var range = parseInt($('#game_range').val());
     Session.set('game_range', range);
+  },
+  'input #results_num' : function(){
+    var results_num = parseInt($('#results_num').val());
+    Session.set('results_num', results_num);
   }
 })
 
