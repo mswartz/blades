64c0d9a10966e13b28a81c882e3023007a5aaf73
Mike Swartz <mswartz@gmail.com>
2014-01-01 14:00:43 -0500

    added a results range to games page

--- games.js
+++ games.js
@@ -11,15 +11,16 @@ G A M E S
 if (Meteor.isClient) {
 
 Session.setDefault('sort_by', 'game_no_asc');
+Session.setDefault('game_range', 1);
 
 Template.games.helpers({
   games : function(){
     var games;
 
     if(Session.get('sort_by')=='game_no_desc'){
-      games = Games.find({}, {sort: {game_no : -1}}).fetch();
+      games = Games.find({game_no: {$gte: Session.get('game_range')}}, {sort: {game_no : -1}}).fetch();
     } else if (Session.get('sort_by')=='game_no_asc') {
-      games = Games.find({}, {sort: {game_no : 1}}).fetch();
+      games = Games.find({game_no: {$gte: Session.get('game_range')}}, {sort: {game_no : 1}}).fetch();
     }
 
     return games;
@@ -36,6 +37,10 @@ Template.games.events({
   },
   'click #game_no_desc' : function() {
     Session.set('sort_by', 'game_no_desc');
+  },
+  'change #game_range' : function() {
+    var range = parseInt($('#game_range').val());
+    Session.set('game_range', range);
   }
 })
 
