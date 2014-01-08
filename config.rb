7c21945cf19826f640ee48207a60bb5ba719b4e9
Mike Swartz <mswartz@gmail.com>
2014-01-07 23:55:39 -0500

    started to style it

---
+++ config.rb
@@ -0,0 +1,15 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "public/css"
+sass_dir = "public/sass"
+images_dir = "public/img"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+relative_assets = true
+add_import_path "public/bower_components/Upbase/components"
+
+line_comments = false
+# output_style = :compact :compressed :nested :expanded
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
