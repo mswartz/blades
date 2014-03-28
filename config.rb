77477b0ce76c0798789f65bdd4e42ed26fda9c1b
Mike Swartz <mswartz@gmail.com>
2014-03-27 22:33:29 -0400

    more styling to player-detail

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 # Require any additional compass plugins here.
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "public/css"
+css_dir = "client/css"
 sass_dir = "public/sass"
 images_dir = "public/img"
 javascripts_dir = "js"
