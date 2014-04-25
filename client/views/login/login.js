17a513efad142256ed9ed52a9cebdc2a3f767b02
George Nishimura <george@upstatement.com>
2014-04-25 16:02:31 -0400

    updated readme, fixed login/registration to include helpful error messages, added page changes after loggin/logout

--- client/views/login/login.js
+++ client/views/login/login.js
@@ -18,11 +18,12 @@ if (Meteor.isClient) {
         if (err) {
           // The user might not have been found, or their password
           // could be incorrect. The login attempt has failed.
+          $(t.find('#login-form')).append('<div class="error-notification">' + (err.reason || 'There was a problem signing in') + '</div>');
           console.log('login error', err);
         } else {
           // The user has been logged in.
           console.log('login successful');
-          Session.set('logged_in', true);
+          Router.go('newgame');
         }
       });
       return false;
@@ -46,18 +47,25 @@ if (Meteor.isClient) {
           Accounts.createUser({ username: username, email: email, password: password }, function(err) {
             if (err) {
               // Inform the user that account creation failed
+              $(t.find('.error-notification')).remove();
+              $(t.find('#register-form')).append('<div class="error-notification">' + (err.reason || 'There was an error') + '</div>');
               console.log('fail', err);
             } else {
               // Success. Account has been created and the user
               // has logged in successfully.
-              Session.set('logged_in', true);
+              Router.go('home');
               console.log('user created');
-              displayChange();
             }
 
           });
         } else {
           console.log('keycheck failed', result);
+          var key_input = t.find('#account-key');
+          key_input.setCustomValidity("The key is not valid");
+          $(key_input).focus(function() {
+            this.setCustomValidity("");
+            $(this).val("");
+          });
         }
 
       })
