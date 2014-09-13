054361d9e434c933ab7be0e2f620d6e3a5ee14e0
George Nishimura <thetmkay@gmail.com>
2014-09-13 06:30:00 -0400

    fix games for gamesort

--- client/views/games/games.js
+++ client/views/games/games.js
@@ -9,16 +9,18 @@ if (Meteor.isClient) {
 Session.setDefault('sort_by', -1);
 
 Template.games.rendered = function() {
-  Session.set('total_games', Games.find().count());
+  Session.set('games_total', Games.find().count());
 };
 
 Template.games.helpers({
   games : function(){
-    var games = Games.find({game_no: {$gte: Session.get('game_range')}}, {sort: {game_no : Session.get('sort_by')}, limit: Session.get('results_num')}).fetch();
+    var start = Session.get('games_start'),
+        end = Session.get('games_end'),
+        games = Splice(start, end, Games.find({}, {sort: {game_no : Session.get('sort_by')}}).fetch());
     return games;
   },
 
-  total_games : function(){
+  games_total : function(){
     var games = Games.find().count();
     Session.set('games_total', games);
   	return games;
